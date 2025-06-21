95929a51a1546ea898b4011d9d0dd23bdeb0ac10
babypandaflash <babypandaflash@gmail.com>
2025-06-21 13:54:30 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: {
+    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
+    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+    DISCORD_REDIRECT_URI: process.env.DISCORD_REDIRECT_URI,
+  },
+};
+
